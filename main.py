42924cc9f8fe4a01bff3b76edf73c0f6a063d3b4
Owen Luts <oluts47@gmail.com>
2022-12-21 21:17:40 -0500

    new file:   .vscode/settings.json 	modified:   __pycache__/main.cpython-310.pyc 	new file:   gravitation.py 	modified:   main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import proj_motion
 import obj_collision
 import pendulum
 import settings
+import gravitation
 
 def start_proj_motion():
     proj_motion.start()
@@ -25,15 +26,18 @@ def start_pendulum():
 def start_settings():
     settings.start()
 
+def start_gravitation():
+    gravitation.start()
+
 def begin():
     pygame.display.set_caption("Physics Simulations")
     menu = pygame_menu.Menu('Physics Simulations: AP Physics C', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_SOLARIZED)
     menu.add.button('Projectile Motion Sim', start_proj_motion)
     menu.add.button('Object Collision Sim', start_obj_collision)
     menu.add.button('Pendulum Sim', start_pendulum)
+    menu.add.button('Gravitation Sim', start_gravitation)
     menu.add.button('Settings', start_settings)
     menu.add.button('Quit', pygame_menu.events.EXIT)
-
     menu.mainloop(WIN)
 
 
